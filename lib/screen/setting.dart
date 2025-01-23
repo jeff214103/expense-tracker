862ea085c86952c5f2bde0293545a38ffc8f8194
Jeff <jeff214113@gmail.com>
2025-01-23 13:41:25 -0800

    Upgrade dart to latest version

--- lib/screen/setting.dart
+++ lib/screen/setting.dart
@@ -32,19 +32,6 @@ class _SettingScreenState extends State<SettingScreen>
     super.dispose();
   }
 
-  void _showErrorSnackBar(String message) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(
-          message,
-          style: const TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Colors.red,
-        behavior: SnackBarBehavior.floating,
-        duration: const Duration(seconds: 3),
-      ),
-    );
-  }
 
   Future<void> _showNumberInputDialog({
     required BuildContext context,
